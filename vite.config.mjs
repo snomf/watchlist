d190efa701177e48ea379b1baa2352b2deaf7c57
Juainny Lopez <93808126+snomf@users.noreply.github.com>
2026-02-09 22:53:45 -0500

    Add sync page and fix build config

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         profile: resolve(__dirname, 'profile.html'),
         stats: resolve(__dirname, 'stats.html'),
+        sync: resolve(__dirname, 'sync.html'),
         callback: resolve(__dirname, 'callback.html'),
         'favorite-removal-modal': resolve(__dirname, 'favorite-removal-modal.html'),
       },
